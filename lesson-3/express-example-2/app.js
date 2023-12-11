285b5f0525428e892ca13b93fdf902ae7604c0d7
unknown <bogdan.lyamzin.d@gmail.com>
2023-12-11 21:30:16 +0200

    finish lesson 3

---
+++ lesson-3/express-example-2/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+import movies from "./movies.js";
+
+const app = express();
+
+app.get("/movies", (req, res)=> {
+    const databaseResponse = null;
+    // res.json(databaseResponse);
+    // res.send(databaseResponse)
+    res.json(movies);
+    // res.send(movies);
+})
+
+app.listen(3000, ()=> console.log("Server running on 3000 PORT"));
