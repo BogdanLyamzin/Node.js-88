db22f0acee039f1b02bbd522e43aabb00bbae0eb
igrok1803444 <v618591@gmail.com>
2024-01-06 13:43:04 +0200

    end hw 04

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,3 +1,4 @@
 export { default as isEmptyBody } from "./isEmptyBody.js";
 export { default as isValid } from "./isValid.js";
 export { default as authorization } from "./authorization.js";
+console.log();
